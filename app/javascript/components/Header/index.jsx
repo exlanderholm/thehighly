b4d8aae4135c5998ecd3911906483d8e5efff8b8
Michael Ray <michaelburtonray@gmail.com>
2017-11-01 18:29:01 -0400

    home page header has no links

--- app/javascript/components/Header/index.jsx
+++ app/javascript/components/Header/index.jsx
@@ -1,20 +1,22 @@
 import React from 'react'
 import { NavLink, Link } from 'react-router-dom'
 import Logo from '../Logo'
+
 import styles from './header.css'
 
-const Header = ({openNavigation}) => (
+
+const Header = ({ openNavigation, isHomePage }) => (
   <header className="header">
     <div className="header__inner">
       <div className="menu-button-area"></div>
 
-      <Link className="mainLink about-link caps" to="/about">About</Link>
+      {!isHomePage && <Link className="mainLink about-link caps" to="/about">About</Link>}
 
       <div className="logo-area">
         <Logo />
       </div>
 
-      <Link className="mainLink caps" to="/recommended">The Highly Recommended</Link>
+      {!isHomePage && <Link className="mainLink caps" to="/recommended">The Highly Recommended</Link>}
 
       <div className="menu-button-area">
         <button className="menu-button"onClick={openNavigation}>Menu</button>
