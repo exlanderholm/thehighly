81eb25cdcc671dcdf3f0b6f278bba48da6d9b2fd
Sasha Kluchnik <me@sashakluchnik.com>
2017-11-15 19:07:08 -0500

    404 styled

--- app/javascript/components/My404Component/index.jsx
+++ app/javascript/components/My404Component/index.jsx
@@ -3,9 +3,9 @@ import styles from './404.css'
 
 const My404Component = () =>
   <div className="not-found">
-    <h1>The page you were looking for doesn't exist.</h1>
+    <h1>404</h1>
+    <h2>The page you were looking for doesn't exist.</h2>
     <p>You may have mistyped the address or the page may have moved.</p>
-    <p>404 page not found</p>
   </div>
 
 export default My404Component
