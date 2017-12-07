27bd32786b9d54479db2e89e2d457a695530ee3a
Sasha Kluchnik <me@sashakluchnik.com>
2017-12-07 14:02:16 -0500

    city page color added

--- app/javascript/components/RecommendedDetail/index.jsx
+++ app/javascript/components/RecommendedDetail/index.jsx
@@ -36,13 +36,17 @@ const RecommendedDetail = ({
       <div className="notice caps" dangerouslySetInnerHTML={createMarkupObject(mapMessaging)} />
 
       <div className="where-to-go">
-        <h4 className="section__title caps">Where to Go</h4>
-        {whereToGo.filter( place => place.contentTypeId === 'place' ).map( place => <Place {...place} key={place.id} /> )}
+        <div className="inner">
+          <h4 className="section__title caps">Where to Go</h4>
+          {whereToGo.filter( place => place.contentTypeId === 'place' ).map( place => <Place {...place} key={place.id} /> )}
+        </div>
       </div>
 
       <div className="what-to-get">
-        <h4 className="section__title caps">What To Get</h4>
-        {whatToGetSorted.map( (group, idx) => <ProductGroup {...group} key={idx} />)}
+        <div className="inner">
+          <h4 className="section__title caps">What To Get</h4>
+          {whatToGetSorted.map( (group, idx) => <ProductGroup {...group} key={idx} />)}
+        </div>
       </div>
     </div>
   )
