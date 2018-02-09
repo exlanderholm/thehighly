34ec5b6e905d0622608fd01eb534ed8afa2bde9b
Raj <rajatverma.me@gmail.com>
2018-02-09 18:53:16 +0530

    # Generated sitemaps via RoR # Added Robots.txt

--- app/controllers/sitemaps_controller.rb
+++ app/controllers/sitemaps_controller.rb
@@ -1,5 +1,12 @@
 class SitemapsController < ApplicationController
-  def show
-    render xml: {termsAndConditions: "test test test"}
+  def index
+    @posts = contentful
+    .entries(content_type: "post", order: '-fields.date')
+    .map do |post|
+      {:slug => post.slug}
     end
+    respond_to do |format|
+      format.xml
+    end
+  end
 end
